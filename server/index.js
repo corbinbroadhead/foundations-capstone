9ba434fc0c3791901384a26fc4433dac4ad9ba6f
Corbin Broadhead <poujmail@gmail.com>
2023-04-27 21:20:48 -0600

    Working through

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(`${__dirname}`));
+app.use(express.static(`${__dirname}/public`));
 
 const { 
     createGameCard,
     getGames,
     deleteGame
 } = require("./controller");
-
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
 app.post(`/api/games`, createGameCard);
 app.get(`/api/games`, getGames);
 app.delete(`/api/games/:id`, deleteGame);
